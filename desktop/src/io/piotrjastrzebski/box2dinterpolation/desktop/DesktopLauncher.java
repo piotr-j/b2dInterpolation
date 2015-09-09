743822981254ed55fb71e1f21de07d8be9f6118f
EvilEntity <me@piotrjastrzebski.io>
2015-09-09 21:20:33 +0200

    fixed things

--- desktop/src/io/piotrjastrzebski/box2dinterpolation/desktop/DesktopLauncher.java
+++ desktop/src/io/piotrjastrzebski/box2dinterpolation/desktop/DesktopLauncher.java
@@ -3,7 +3,6 @@ package io.piotrjastrzebski.box2dinterpolation.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import io.piotrjastrzebski.box2dinterpolation.B2DInterp;
-import io.piotrjastrzebski.box2dinterpolation.Box2dInterpolationScreen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
